e1e10047070868095f57e341cd889131936d1f47
23Norm <114043352+23Norm@users.noreply.github.com>
2023-11-08 23:31:11 +0800

    Add files via upload

---
+++ criminalintent/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package com.example.criminalintent;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected CrimeListFragment createFragment() {
+        return new CrimeListFragment();
+    }
+
+}
