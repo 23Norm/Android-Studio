deeed0a58a1cf9c620914138a1d210f8c37c6b04
Norman Jason <114043352+23Norm@users.noreply.github.com>
2023-12-13 11:59:25 +0800

    Update Crime.java
    Added the method "getFormattedTime"

--- criminalintent/Crime.java
+++ criminalintent/Crime.java
@@ -1,19 +1,20 @@
-package com.example.criminalintent;
+package com.example.crimeintent;
 
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.UUID;
 
 public class Crime {
-
     private UUID mId;
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private boolean mRequiresPolice = false;
     private final String formattedDate;
 
-    private boolean mRequiresPolice;
 
 
     public Crime() {
@@ -22,46 +23,45 @@ public class Crime {
 
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL);
         formattedDate = dateFormat.format(mDate);
-
     }
-
-    public boolean requiresPolice() {
-        return mRequiresPolice;
+    public UUID getId() {
+        return mId;
     }
-
-    public void setRequiresPolice(boolean requiresPolice) {
-        mRequiresPolice = requiresPolice;
+    public String getTitle() {
+        return mTitle;
     }
 
     public String getFormattedDate() {
         return formattedDate;
     }
+    public String getFormattedTime() {
+        String format = "HH:mm";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format, Locale.US);
 
-    public UUID getId() {
-        return mId;
+        return simpleDateFormat.format(mDate);
     }
-
-    public String getTitle() {
-        return mTitle;
-    }
-
     public void setTitle(String title) {
         mTitle = title;
     }
-
-    public Date getDate() {
-        return mDate;
-    }
-
     public void setDate(Date date) {
         mDate = date;
+    }
+    public Date getDate() {
+        return mDate;
     }
 
     public boolean isSolved() {
         return mSolved;
     }
-
     public void setSolved(boolean solved) {
         mSolved = solved;
     }
+
+    public boolean requiresPolice() {
+        return mRequiresPolice;
+    }
+    public void setRequiresPolice(boolean requiresPolice) {
+        mRequiresPolice = requiresPolice;
+    }
+
 }
