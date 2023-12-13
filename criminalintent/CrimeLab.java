92c2a9a7029779040fcf0d1247467e1dfde8cb1f
Norman Jason <114043352+23Norm@users.noreply.github.com>
2023-12-13 12:01:13 +0800

    Update CrimeLab.java

--- criminalintent/CrimeLab.java
+++ criminalintent/CrimeLab.java
@@ -1,46 +1,40 @@
-package com.example.criminalintent;
+package com.example.crimeintent;
 
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+
+    private LinkedHashMap<UUID, Crime> mCrimes;
 
     public static CrimeLab get(Context context) {
-
         if (sCrimeLab == null) {
             sCrimeLab = new CrimeLab(context);
         }
-
         return sCrimeLab;
     }
-
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        mCrimes = new LinkedHashMap<>();
+        for (int i = 1; i <= 20; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            crime.setRequiresPolice(i % 2 == 0);
-            mCrimes.add(crime);
+            crime.setSolved(i % 4 == 0);
+            crime.setRequiresPolice(true);
+            mCrimes.put(crime.getId(), crime);
         }
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>(mCrimes.values());
     }
-
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
-
-        return null;
+        return mCrimes.get(id);
     }
 }
